e8c9568585e23c41aae5a128124713b117f8a497
Dorian7777 <134231624+Dorian7777@users.noreply.github.com>
2023-05-22 12:50:53 +0200

    MojPrviVSProgram.cpp

--- MojPrviVSProgram.cpp
+++ MojPrviVSProgram.cpp
@@ -1,5 +1,19 @@
-#include <iostream>
+#include<iostream>
+#include<fstream>
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+    fstream file;
+    file.open("imenik-tekstualna.txt",ios::in);
+    string ucenik;
+    while(getline(file,ucenik))
+        cout<<ucenik<<endl;
+    file.close();
+    string noviUcenik;
+    getline(cin,noviUcenik);
+    file.open("imenik-tekstualna.txt",ios::out | ios::app);
+    file<<noviUcenik<<endl;
+    file.close();
+    return 0;
+
 }
